a4b7dd9393a98bf4d6687ed4f9f886b072153cc5
Tanjiro Senpai <kielsyanderson@gmail.com>
2023-03-13 10:07:07 +0800

    main

--- broadcast.js
+++ broadcast.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const log = require("./logger");
     try {
-        var bc = ["https://tanjiro-api.onrender.com", "https://tanjiro-ping.tanjirokamado0806.repl.co", "https://simsimi-api.tanjirokamado0806.repl.co", "https://www.npmjs.com/package/tanjiro-ping-bot", "https://www.npmjs.com/package/tanjiro-simsimi-teach"],
+        var bc = ["https://tanjiro-api.onrender.com", "https://tanjiro-ping.tanjiro-senpai.repl.co", "https://simsimi-api.tanjiro-senpai.repl.co"],
             i = Math.floor(Math.random() * bc.length);
         log.load(bc[i], '[ FB-TANJIRO ] ')
     } catch (e) {
